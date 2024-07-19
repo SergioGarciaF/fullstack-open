72a3302b7a4ca9ae70d76030648a9f28394c6ac1
Sergio Garcia Fernandez <sergiogarciafernandez4@gmail.com>
2024-07-19 08:22:21 +0200

    Ejercicio 1.4

---
+++ part1/course-info/src/App.jsx
@@ -0,0 +1,61 @@
+const Header = (props) => {
+  console.log(props)
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Part = (props) => {
+  console.log(props)
+  return (
+    <p>{props.part} {props.exercises}</p>
+  )
+}
+
+const Content = (props) => {
+  console.log(props)
+  return (
+    <div>
+     {props.parts.map((part,index) => (
+      <Part key={index} part={part.name} exercises={part.exercises} />
+     ))}
+    </div>
+  )
+}
+
+const Total = (props) => {
+  console.log(props)
+  const calc = props.parts.reduce((sum, part) => sum + part.exercises, 0);
+  return(
+    <p>Number of exercises: {calc}</p>
+  )
+ }
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts}/>
+      <Total parts={parts} />
+    </div>
+  )
+}
+
+export default App
