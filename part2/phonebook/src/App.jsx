6c35fbeb2986abd1fb3bd7c5d6362f43356fe46a
Sergio Garcia Fernandez <sergiogarciafernandez4@gmail.com>
2024-08-01 10:50:22 +0200

    2.6

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [newName, setNewName] = useState('')
+
+  const addName = (e) => {
+    e.preventDefault();
+    const personObject = {
+      name: newName
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+    console.log(persons)
+  }
+
+  const handleNewName = (e) => {
+    setNewName(e.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNewName} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map((person, index) => (
+          <p key={index}>{person.name}</p>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default App
