2e546d5cbf1f9926db6390b2e540aab17e7e28c4
Sergio Garcia Fernandez <sergiogarciafernandez4@gmail.com>
2024-08-01 09:13:34 +0200

    2.4

--- part2/course-info/src/components/Course.jsx
+++ part2/course-info/src/components/Course.jsx
@@ -6,35 +6,35 @@ const Header = ({ name }) => {
 
 const Content = ({ parts }) => {
   return (
-    parts.map(part => (
-      <Part key={part.id} name={part.name} exercises={part.exercises}/>
-    ))
-  )
-}
-
-const Part = ({name, exercises}) => {
-  return(
     <>
-      <p>{name} {exercises}</p> 
+      {parts.map(part => (
+        <Part key={part.id} name={part.name} exercises={part.exercises} />
+      ))}
     </>
-  )
-}
+  );
+};
 
-const Total = ({parts}) => {
+const Part = ({ name, exercises }) => {
+  return (
+    <p>{name} {exercises}</p>
+  );
+};
+
+const Total = ({ parts }) => {
   const totalExercises = parts.reduce((acc, sum) => {
     return acc + sum.exercises;
-  },0)
-  return(
+  }, 0)
+  return (
     <p>The course has {totalExercises} exercises.</p>
   )
-} 
+}
 
 
 const Course = ({ course }) => {
   return (
     <>
       <Header name={course.name} />
-      <Content parts={course.parts}/>
+      <Content parts={course.parts} />
       <Total parts={course.parts} />
     </>
   )
