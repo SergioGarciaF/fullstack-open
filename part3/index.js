f3596f3536d544b3952c0b1e57cc7e24dcd04b6e
Sergio Garcia Fernandez <sergiogarciafernandez4@gmail.com>
2024-08-06 19:13:35 +0200

    3.4

--- part3/index.js
+++ part3/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     { 
       "id": 1,
@@ -24,11 +26,11 @@ let persons = [
     }
 ]
 
-app.get('/api/agenda/persons', (request, response) => {
+app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
-app.get('/api/agenda/info', (request, response) => {
+app.get('/api/info', (request, response) => {
     const length = persons.length;
     const date = new Date();
     const htmlResponse = `
@@ -38,7 +40,7 @@ app.get('/api/agenda/info', (request, response) => {
     response.send(htmlResponse).end();
 });
 
-app.get('/api/agenda/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     const person = persons.find(person => person.id === id);
 
@@ -49,21 +51,43 @@ app.get('/api/agenda/persons/:id', (request, response) => {
     }
 })
 
-app.delete('/api/agenda/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id);
     
     const personIndex = persons.findIndex(person => person.id === id);
     
     if (personIndex !== -1) {
         persons = persons.filter(person => person.id !== id);
-        console.log(`Person with id: ${id} deleted succesfully.`);
         response.status(204).end();
     } else {
-        console.log(`Person with id: ${id} not found.`);
         response.status(404).json({ error: 'Person not found' });
     }
 });
 
+const generateId = () => {
+    return Math.floor(Math.random() * 1000000000);
+};
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body;
+    console.log('Received body:', body);
+
+    if (!body.name || !body.number) {
+        return response.status(400).json({ 
+            error: 'name or number is missing' 
+        });
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId(body.id)
+    };
+
+    persons = persons.concat(person);
+
+    response.json(person);
+});
 
 const PORT = 3001
 app.listen(PORT)
